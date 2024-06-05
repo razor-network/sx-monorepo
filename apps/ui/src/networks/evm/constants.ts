4ba671bdbc73306439102df83966b478e988aa58
rajkharvar <rajkharvar1808@gmail.com>
2024-06-05 10:45:49 +0530

    chore: update execution strategy address and space address

--- apps/ui/src/networks/evm/constants.ts
+++ apps/ui/src/networks/evm/constants.ts
@@ -22,9 +22,9 @@ export const MERKLE_VOTING_STRATEGY_ADDRESS = evmNetworks['skale-mainnet'].Strat
 export const COOLDOWN_PERIOD = 10; // * seconds
 export const MAX_ACTIVE_PROPOSALS = 100;
 // !NOTE: Update this after space deployed
-export const EXECUTION_STRATEGY_ADDRESS = '0x093758ba612D90f14Dd702Df9020435DC4Daf000';
+export const EXECUTION_STRATEGY_ADDRESS = '0xdF2Fb38249640C1D7ee6f0347970C4D3059f8D63';
 export const QUORUM = '10000000000000000000000000'; // * 10 million
-export const SPACE_ADDRESS = '0x14Fd5d36a0E234C623A235756C1e2B9D9135a6b8';
+export const SPACE_ADDRESS = '0x579f0121f678Da51a66fFC0bf6059fF87f9045E5';
 export const REDIRECT_URL = `/skale-mainnet:${SPACE_ADDRESS}`;
 
 export function createConstants(networkId: NetworkID) {
